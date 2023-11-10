174756a0ca9a5a3be23a1c11c1d42e8846e6a1b9
Bilal Nasir <95551739+noshotbigjuicersbilal151@users.noreply.github.com>
2023-11-09 21:32:06 -0800

    Update searchAgents.py

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -302,7 +302,10 @@ class CornersProblem(search.SearchProblem):
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        return len(state[1]) == 4
+        if len(state[1]) == 4:
+            return True
+        else:
+            return False
         
 
     def getSuccessors(self, state):
